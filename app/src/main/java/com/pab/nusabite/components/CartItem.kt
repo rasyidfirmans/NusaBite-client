eb8c3c35fa30f27841e67428a2092d95f444d511
syifa135 <syifanuraini@student.uns.ac.id>
2025-06-28 22:29:12 +0700

    Mengambil direktori components dari fix/main-category-cart

--- app/src/main/java/com/pab/nusabite/components/CartItem.kt
+++ app/src/main/java/com/pab/nusabite/components/CartItem.kt
@@ -1,98 +1,50 @@
-package com.pab.nusabite.components
+package com.pab.nusabite.components.order
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.pab.nusabite.network.BASE_URL
 import com.pab.nusabite.utils.dataclass.CartItem
 import java.text.NumberFormat
 import java.util.Locale
 
 @Composable
-fun CartItem(item: CartItem) {
-    Row (
+fun CartItemCard(item: CartItem) {
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
             .clip(MaterialTheme.shapes.medium)
-            .clickable { /* Handle item click */ }
-            .padding(horizontal = 16.dp, vertical = 16.dp),
+            .clickable { }
+            .padding(16.dp)
     ) {
-        Image(
-            painter = painterResource(item.image),
+        AsyncImage(
+            model = "http://10.0.2.2:8000/${item.image}",
             contentDescription = item.name,
             modifier = Modifier
-                .clip(shape = MaterialTheme.shapes.medium)
+                .size(100.dp)
+                .clip(MaterialTheme.shapes.medium)
         )
-        Column (
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, end = 8.dp, top = 8.dp, bottom = 8.dp)
+                .padding(start = 16.dp)
+                .fillMaxHeight()
                 .weight(1f)
         ) {
-            Text(
-                text = item.name,
-                style = MaterialTheme.typography.bodyLarge,
-                fontWeight = FontWeight.Bold,
-            )
+            Text(text = item.name, style = MaterialTheme.typography.bodyLarge)
             Text(
                 text = NumberFormat.getCurrencyInstance(Locale("id", "ID")).format(item.price),
-                fontWeight = FontWeight.Bold,
                 color = Color(254, 140, 0),
-                modifier = Modifier
-                    .weight(1f)
+                style = MaterialTheme.typography.bodyMedium
             )
-            Row (
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Counter(
-                    modifier = Modifier
-                        .weight(1f)
-                )
-                Icon(
-                    imageVector = Icons.Default.Delete,
-                    contentDescription = "Delete Item",
-                    tint = Color.Red,
-                    modifier = Modifier
-                        .clip(CircleShape)
-//                        .background(color = Color(230 / 255f, 230 / 255f, 230 / 255f, 0.7f))
-                        .clickable { /* Handle delete action */ }
-                        .padding(8.dp)
-                )
-            }
+            Text("Qty: ${item.quantity}", style = MaterialTheme.typography.bodySmall)
         }
     }
-}
-
-@Preview (showBackground = true)
-@Composable
-fun CartItemPreview() {
-    CartItem(
-        item = CartItem(
-            name = "Burger With Meat",
-            price = 15000.0,
-            quantity = 2,
-            image = com.pab.nusabite.R.drawable.lumpia
-        )
-    )
 }
